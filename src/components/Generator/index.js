8647a7c2910e63065ab006e330fb5b4ec53dc7ed
nima habibkhoda <nima.2004hkh@gmail.com>
2019-10-11 23:48:21 +0330

    fix bug

--- src/components/Generator/index.js
+++ src/components/Generator/index.js
@@ -5,27 +5,27 @@ import {useStateValue} from '../../statemanagement'
 function Generator(props) {
   const [text, setText] = useState();
   const [{errors}, dispatch] = useStateValue();
-  let InnerText=errors !== "" ? errors : text;
 
   const generateNewText=useCallback(()=>{
     let params = {
       sentences: 1,
       lorem: 0
     };
+
     generateText(params).then(res => {
       const {data} = res
       dispatch({type: 'changeText', text: data})
       setText(data);
     })
+
   },[dispatch])
 
   useEffect(() => {
     generateNewText()
   }, [generateNewText])
-
   return (<React.Fragment>
     <div className="generatedText">
-      <span dangerouslySetInnerHTML={{__html: InnerText}} />
+      <span dangerouslySetInnerHTML={{__html: (errors !== "" && errors !==undefined) ? errors : text}} />
     </div>
 
   </React.Fragment>)
