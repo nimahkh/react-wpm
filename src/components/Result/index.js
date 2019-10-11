720b5052cb3700b02980aee285fc67665b262d44
nima habibkhoda <nima.2004hkh@gmail.com>
2019-10-11 23:47:29 +0330

    add Results of wpm inside component

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {useStateValue} from "../../statemanagement"
 
 function Result(props){
-  const [, dispatch] = useStateValue();
+  const [{testResult:WPMResult}, dispatch] = useStateValue();
 
   function reTest(){
     dispatch({
       type:'stopTimer',
       stop:false
     })
+    dispatch({
+      type:'changeErrors',
+      stop:''
+    })
   }
 
   return(
-    <div onClick={reTest}>Your time is finished</div>
+    <React.Fragment>
+      <div>
+        <span>WPM : {WPMResult.wpm}</span>
+        <span>Elapsed Time : {WPMResult.elapsedTime}</span>
+        <span>Misspeled Node : {WPMResult.misspelled}</span>
+        <span>Accuracy Node : {WPMResult.accuracy}</span>
+      </div>
+      <div onClick={reTest}>Your time is finished</div>
+    </React.Fragment>
   )
 }
 
